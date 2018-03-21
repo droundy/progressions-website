781152a5241ee85514066219fc8bbf2d9e35949d
David Roundy <roundyd@physics.oregonstate.edu>
2018-03-21 12:29:54 -0700

    have upload.sh grab spreadsheet from box

--- upload.sh
+++ upload.sh
@@ -2,11 +2,14 @@
 
 set -ev
 
-fac -v
+# copy latest version of the learning progression from box
+cp ~/box/Learning\ Progressions\ for\ Partial\ Derivatives/Learning\ Progression\ Database.xlsx progression.xlsx
+
+fac
 
 SITEDIR=public_html/progression
 
-ssh science.oregonstate.edu "rm -rfv $SITEDIR"
+ssh science.oregonstate.edu "rm -rf $SITEDIR"
 ssh science.oregonstate.edu "mkdir -p $SITEDIR"
 
 scp output/* science.oregonstate.edu:$SITEDIR/
