d01c1a126216d54b6e268744fe8eb19a74ef8213
David Roundy <roundyd@physics.oregonstate.edu>
2018-08-01 11:29:54 -0700

    remove some comments from upload.sh

--- upload.sh
+++ upload.sh
@@ -20,10 +20,3 @@ ssh science.oregonstate.edu "rm -rf $SITEDIR"
 ssh science.oregonstate.edu "mkdir -p $SITEDIR"
 
 scp -r output/* science.oregonstate.edu:$SITEDIR/
-
-# rsync -v *.svg *.png *.py *.html *.css $SITE/
-# rsync -v figs/*.svg $SITE/figs/
-
-# rm -f hw/solution*
-# rsync -v -r hw/*.pdf $SITE/hw/
-
