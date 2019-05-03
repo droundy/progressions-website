876d30f2e193fbf1b8206850ef9cc978d02072f4
David Roundy <roundyd@physics.oregonstate.edu>
2019-05-03 09:41:54 -0700

    work on making mirror work properly

--- upload.sh
+++ upload.sh
@@ -2,21 +2,27 @@
 
 set -ev
 
-if test -e ~/box/Learning\ Progressions\ for\ Partial\ Derivatives/Learning\ Progression\ Database.xlsx; then
-    # copy latest version of the learning progression from box
-    rsync -a ~/box/Learning\ Progressions\ for\ Partial\ Derivatives/Learning\ Progression\ Database.xlsx progression.xlsx
-    rsync -a ~/box/Learning\ Progressions\ for\ Partial\ Derivatives/Figures/* figs/
-    git add figs
-fi
+# if test -e ~/box/Learning\ Progressions\ for\ Partial\ Derivatives/Learning\ Progression\ Database.xlsx; then
+#     # copy latest version of the learning progression from box
+#     rsync -a ~/box/Learning\ Progressions\ for\ Partial\ Derivatives/Learning\ Progression\ Database.xlsx progression.xlsx
+#     rsync -a ~/box/Learning\ Progressions\ for\ Partial\ Derivatives/Figures/* figs/
+#     git add figs
+# fi
 
 # wget -O libraries/MathJax.js https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_HTML
 # wget -O libraries/jquery.js https://code.jquery.com/jquery-3.3.1.min.js
 
-fac
+# fac
 
 SITEDIR=public_html/progression
 
 ssh science.oregonstate.edu "rm -rf $SITEDIR"
 ssh science.oregonstate.edu "mkdir -p $SITEDIR"
 
-scp -r output/* science.oregonstate.edu:$SITEDIR/
+scp -r mirror/* science.oregonstate.edu:$SITEDIR/
+
+scp style.css science.oregonstate.edu:$SITEDIR/
+
+scp -r libraries science.oregonstate.edu:$SITEDIR/
+
+scp -r figs science.oregonstate.edu:$SITEDIR/
